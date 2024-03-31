c74ac822b47b13d7dae5e63844be9babd38b18bc
Shirish-creator <84584836+Shirish-creator@users.noreply.github.com>
2024-03-31 19:06:40 -0400

    post processing added

--- src/components/Macintosh.js
+++ src/components/Macintosh.js
@@ -1,24 +1,86 @@
 import React from 'react';
 import { useGLTF, useTexture,Html } from '@react-three/drei';
-import { MeshStandardMaterial } from 'three';
+import { Bloom, DepthOfField, EffectComposer, Glitch, Noise, Vignette } from '@react-three/postprocessing';
 import * as THREE from 'three';
+import { useFrame } from "react-three-fiber";
+import { useRef,useState } from 'react';
+import { OrbitControls } from '@react-three/drei';
+import { TransformControls, PivotControls,Environment } from '@react-three/drei';
+import { useControls } from 'leva';
+import { useLoader } from 'react-three-fiber';
+import { PresentationControls } from '@react-three/drei';
+import {BlendFunction} from 'postprocessing'
 
+const Macintosh = (props,ref) => {
+  const mesh = useRef();
+  const [active, setActive] = useState(false);
 
-
-const Macintosh = (props) => {
   const { nodes } = useGLTF('/MacintoshCell.glb');
-  const standardMaterial = new MeshStandardMaterial({
-    color: 0xffffff, // Adjust color as needed
-    roughness: 0.5, // Adjust roughness as needed
-    metalness: 0.5, // Adjust metalness as needed
-  });
+console.log(nodes)
+ 
   const bakedTexture=useTexture('/BAKEDMACINTOSH2.jpg')
   const bakedPlaneTexture=useTexture('/planebaked.jpg')
+          // position={[0,  active ? 4:5.8, 8]}
 
-console.log(nodes)
+
+
+useFrame(() => {
+  mesh.current.position.set(0,  active ? -2:0, active ? -3 :-13);
+});
+
+useFrame((state,delta) => {
+  // const angle=state.clock.elapsedTime
+  // state.camera.position.x=Math.sin(angle)*12;
+  // state.camera.position.z=Math.cos(angle)*12;
+});
+
   return (
     <>
-    <group  position={[0,0,-13]}>
+    
+      {/* <OrbitControls
+          minAzimuthAngle={-Math.PI / 2}
+          maxAzimuthAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 5}
+          maxPolarAngle={Math.PI / 0}
+          minDistance={3}
+          maxDistance={50}
+          zoom0={false}
+        /> */}
+        <EffectComposer multisampling={0}>
+          <Vignette
+          offset={0.3}
+          darkness={0.8}/>
+          <Glitch
+          delay={[4,4]}
+          duration={[0.01, 0.03]}
+          strength={[2,2]}
+          />
+          {/* <DepthOfField
+          focusDistance={0.025}
+          focalLength={0.025}
+          bokehScale={3}
+          /> */}
+          <Noise
+          
+          blendFunction={BlendFunction.MULTIPLY}
+          />
+        </EffectComposer>
+         <ambientLight intensity={1} position={[1, 1, 1]} />
+         
+          <directionalLight position={[200, 500, 1]} intensity={1} />
+          <directionalLight position={[-0.986, 0.817, -0.056]} intensity={.4} />
+          <hemisphereLight skyColor="#b1e1ff" intensity={0.4} groundColor={"#000000"} />
+
+
+          <PresentationControls 
+          
+          global
+          config={{mass:2, tension:300}}
+          >
+    <group ref={mesh} 
+     position={[0,0,-13]}
+     >
+
     {/* <mesh  scale={1.2} position={[1,1,1]}   geometry={nodes.screen.geometry}
         >
           
@@ -26,25 +88,65 @@ console.log(nodes)
 
       {[...Array(11).keys()].map((index) => (
         <mesh 
+        
         key={index} geometry={nodes.MAC.children[index].geometry} >
+          
           <meshBasicMaterial map={bakedTexture} map-flipY={false} ></meshBasicMaterial>
         </mesh>
       ))}
       <mesh
-      rotation={[0,Math.PI, 0]} // Rotate 180 degrees around the Y axis
-
-    scale={70} 
-    geometry={nodes.Plane.geometry}
-    position={[0,-7.2,-10]}
-    >
+        rotation={[0,Math.PI, 0]} // Rotate 180 degrees around the Y axis
+        scale={70} 
+        geometry={nodes.Plane.geometry}
+        position={[0,-7.2,-10]}
+      >
       <meshBasicMaterial map={bakedPlaneTexture} map-flipY={true} ></meshBasicMaterial>
     </mesh>
-   
+    <Html 
+          style={{userSelect:"none",WebkitUserSelect:"none"}}
+          transform
+          wrapperClass='htmlScreen'
+         position={[0, active ? 5.3:6.2,active ?7.1:8.4]}
+          // position={[0,  active ? 4:5.8, 8]}
+          >
+            
+          <button className='iconButton' style={{color:"white"}} onClick={() => setActive(!active)}>
+            {active ?
+         <svg
+         xmlns="http://www.w3.org/2000/svg"
+         fill="none"
+         viewBox="0 0 24 24"
+         strokeWidth="1.5"
+         stroke="currentColor"
+         className="w-6 h-6"
+       >
+         <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+       </svg>
+       
+          :
+          <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M7.5 3.75H6A2.25 2.25 0 0 0 3.75 6v1.5M16.5 3.75H18A2.25 2.25 0 0 1 20.25 6v1.5m0 9V18A2.25 2.25 0 0 1 18 20.25h-1.5m-9 0H6A2.25 2.25 0 0 1 3.75 18v-1.5M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+          />
+        </svg>
+
+          }</button>
+
+          </Html>
      <mesh  position={[-0.15, 2, 6.5]} rotation={[-0.13,0,0]}> 
           <planeGeometry args={[8, 7]} /> 
-          <meshBasicMaterial side={THREE.FrontSide} /> Set basic material with red color
+          <meshBasicMaterial  toolMapped={false} /> Set basic material with red color
           <Html 
-          style={{userSelect:"none",WebkitUserSelect:"none"}}
+          style={{userSelect:"none",WebkitUserSelect:"none",transformStyle: "preserve-3d"}}
           transform
           wrapperClass='htmlScreen'
           
@@ -58,8 +160,12 @@ console.log(nodes)
           </Html>
         </mesh>
     </group>
+    </PresentationControls>
+    {/* <primitive object={nodes.Scene}>
+    <meshBasicMaterial map={bakedTexture} map-flipY={false} ></meshBasicMaterial>
+
+    </primitive> */}
     
-        
 
     </>
   );
--- src/components/threedworld.js
+++ src/components/threedworld.js
@@ -1,54 +1,38 @@
-import { Canvas } from "react-three-fiber";
+import { Canvas, useThree } from "react-three-fiber";
 import { Suspense } from "react";
 import Loader from "./Loader";
 import Macintosh from "./Macintosh"
-import {  OrthographicCamera, Stats, OrbitControls } from '@react-three/drei'
-import { PerspectiveCamera } from "@react-three/drei";
+import React, { useRef } from "react";
+import { useFrame } from "react-three-fiber";
+import * as Three from 'three';
 
 export default function Threed() {
- 
-  return (
-    <>
-    
-    
-    <Canvas 
-            style={{ height: "100vh" }} // Set background color to black
-            
-    className="w-full h-screen "
-   
-    >
-      <perspectiveCamera
-          fov={1000}
-         
-          position={[0,2, 0]}
-          ></perspectiveCamera>
-      <OrbitControls
-  minAzimuthAngle={-Math.PI / 2}
-  maxAzimuthAngle={Math.PI / 2}
-  minPolarAngle={Math.PI / 5}
-  maxPolarAngle={Math.PI / 0}
-  minDistance={2} // Set your minimum distance here
-  maxDistance={100} // Set your maximum distance here
-/>
-<color args={['#000000']} attach="background"></color>
-        <Suspense fallback={<Loader/>}>
-          
-      <ambientLight intensity={1} position={[1,1,1] }/>
-       <directionalLight position={[200,500,1] } intensity={1}/>
-      <directionalLight position={[-0.986, 0.817, -0.056] } intensity={.4}/>
 
+
+return (
+    <>
+      <Canvas 
+      gl={{
+        antialias:true,
     
+      }}
+        style={{ height: "100vh" }}
+        className="w-full h-screen "
+        camera={{
+          fov:95,
+          near:0.1,
+          far:1000,
+          position:[0,1,8]
+        }}
+      >
+       
+      
+        <color args={['#000000']} attach="background" />
+        <Suspense fallback={<Loader/>}>
      
-       <hemisphereLight skyColor="#b1e1ff" intensity={0.4} groundColor={"#000000"}/> 
-    
-      <Macintosh />
-    </Suspense>
-    </Canvas>
-    
-    
-    
-    
+          <Macintosh />
+        </Suspense>
+      </Canvas>
     </>
   );
 }
-
