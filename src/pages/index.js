fc2b5417a45afce342bffd31063554e1fd26c278
Shirish-creator <84584836+Shirish-creator@users.noreply.github.com>
2024-04-04 02:32:02 -0400

    linting error fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,9 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Head from "next/head";
-import { Canvas } from "react-three-fiber";
 import Threed from "@/components/threedworld";
-import { Html } from "@react-three/drei";
 
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
