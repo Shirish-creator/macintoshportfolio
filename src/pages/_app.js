b134daaacb4c2bd52f8607c4ac225a06d67ad0e2
Shirish-creator <84584836+Shirish-creator@users.noreply.github.com>
2024-04-07 10:47:05 -0400

    analytics added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
 import "@/styles/style.scss"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+  <>
+   <Analytics/>
+  <Component {...pageProps} />
+  </>
+ )
+  
 }
