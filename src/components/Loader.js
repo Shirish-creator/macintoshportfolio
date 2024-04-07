50b52c061361c15f0c4deadfbead42eed5e49af1
Shirish-creator <84584836+Shirish-creator@users.noreply.github.com>
2024-04-07 00:11:00 -0400

    loader stat fixed

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -4,20 +4,23 @@ import { useProgress } from '@react-three/drei'
 export default function LoadingScreen() {
     const { progress } = useProgress()
     const [loadedScreen, setLoadedScreen] = useState(false);
-const percent=progress.toFixed(0);
+    const [currentProgress, setCurrentProgress] = useState(0);
 
     useEffect(() => {
         if (progress === 100) {
             setLoadedScreen(true)
         }
+        setCurrentProgress(progress); // Update current progress
     }, [progress])
+
+    // Conditionally render the loading screen only on the client side
 
     return (
         <section>
             <div className={`LoaderScreen 
             ${loadedScreen ? 'LoaderScreen-active' : 'LoaderScreen-inactive'}
              flex fixed justify-center items-center flex-row`} >
-                LOADING  {percent} %
+                LOADING {currentProgress.toFixed(0)} %
             </div>
         </section>
     )
