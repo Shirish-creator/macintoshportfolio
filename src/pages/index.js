e7350bd4dc51f60a1aade90cc447ed3702444211
Shirish-creator <84584836+Shirish-creator@users.noreply.github.com>
2024-04-25 17:38:26 -0400

    removed hj

--- src/pages/index.js
+++ src/pages/index.js
@@ -69,18 +69,7 @@ const [cameraStart,setCameraStart]=useState(false)
     <>
     <Head>
     <title>SHIRISH's Macintosh</title>
-    <script>
-          {`
-           (function(h,o,t,j,a,r){
-            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:4958080,hjsv:6};
-            a=o.getElementsByTagName('head')[0];
-            r=o.createElement('script');r.async=1;
-            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-            a.appendChild(r);
-        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-          `}
-        </script>
+   
     </Head>
     {isMusicPlaying && (
         <audio ref={(element) => setAudioElement(element)} autoPlay loop>
